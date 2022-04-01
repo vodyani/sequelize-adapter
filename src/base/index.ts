94d7cbf177e8b1c5c8a5d674e26181a57a283c27
John Denver <chogathkk@gmail.com>
2022-04-01 18:28:38 +0800

    style: code

--- src/base/index.ts
+++ src/base/index.ts
@@ -1,5 +1,4 @@
 export * from './sequelize-adapter';
-export * from './sequelize-decorator';
 export * from './sequelize-entity-container';
 export * from './sequelize-entity';
 export * from './sequelize';
