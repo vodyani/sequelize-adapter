c7511556259efff3bcf28ded0b2a2e235b82623d
John Denver <chogathkk@gmail.com>
2022-04-09 09:42:17 +0800

    style: code 🌈

--- src/base/sequelize-entity.ts
+++ src/base/sequelize-entity.ts
@@ -4,7 +4,7 @@ import { Table } from 'sequelize-typescript';
 import { pagination } from '../method';
 
 @Table({ freezeTableName: true, timestamps: false })
-export class BaseEntity<T> extends Model<T> {
+export class BaseEntity<T = any, T2 = any> extends Model<T, T2> {
   /**
    * @param options PaginationQueryOptions
    *
